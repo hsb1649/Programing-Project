4ae67741a6ce5d3d0678a8dea149867430cac346
swp1234 <swp5516@naver.com>
2015-05-17 00:29:11 +0900

    idcheck 추가

--- ProgramProject/src/GUI/Main.java
+++ ProgramProject/src/GUI/Main.java
@@ -1,9 +1,14 @@
 package GUI;
 
+import java.sql.SQLException;
+
+import database.DB;
+
 
 public class Main 
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		new Menu1();
+		DB.Access();
 	}
 }
